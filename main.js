7f43ed3678aa420dfe58043ff0d4696673ef3304
Yashraj Dawkhar <dawkharyashraj@gmail.com>
2024-03-29 13:20:28 +0530

    delete 1 file and update 5 files

--- main.js
+++ main.js
@@ -1,65 +1,47 @@
 import './style.css'
 
-document.getElementsByClassName('buy-now-button')[0].addEventListener('click', () => {
-
-    let obj = {
-        method: 'addNewPaymentIframe_send_to_shopify',
-        data: {
-            landingPage: true,
-            requiredJSON: {
-                "items": [
-                    {
-                        image: 'https://cdn.shopify.com/s/files/1/0735/0298/2435/files/71zlbYH749L._SL1500.jpg?v=1694430917',
-                        title: 'Octopus Crawling Baby Toy',
-                        quantity: 1,
-                        price: 129900,
-                        variant_id: 46214569984291
-                    }
-                ],
-                original_total_price: 129900,
-                total_price: 129900
-            }
-
-
-        }
-    }
-
-    parent.postMessage(obj, "*");
-
-})
-
-window.intelliassist_static = {
-    review: true,
-    faq: true,
-    instagram_tag: true
-}
-
-
-window.staticVariables.client.cart_url =`https://${staticVariables.client.shopName}/cart/46214569984291:1`
-
 
 window.meta = {
+
     "product": {
-        "id": 8578795536675,
-        "gid": "gid://shopify/Product/8578795536675",
-        "vendor": "HelpMeShop",
-        "type": "Baby Toys",
+        "id": 7979277123832,
+        "gid": "gid://shopify/Product/7979277123832",
+        "vendor": "Roposo Clout",
+        "type": "",
         "variants": [
             {
-                "id": 46214569984291,
-                "price": 129900,
-                "name": "Octopus Crawling Baby Toy",
+                "id": 43772270510328,
+                "price": 58900,
+                "name": "Electronic LED Mosquito Killer Machine trap lamp With USB",
                 "public_title": null,
-                "sku": ""
+                "sku": "8318353215"
             }
         ]
     },
     "page": {
         "pageType": "product",
         "resourceType": "product",
-        "resourceId": 8578795536675
+        "resourceId": 7979277123832
     }
+
 }
 
 
+window.intelliassist_static_pages = {
+    customerReview: {
+        enable: true,
+        class: 'IT_product_review',
+    },
+    FAQ: {
+        enable: true,
+        class: 'IT_product_FAQ',
+    },
+    checkoutToShopify: {
+        enable: false,
+    },
+    chatbot: {
+        enable: true,
+    }
+
+}
 
